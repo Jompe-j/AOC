9d4e8cabbf9f7216d7474f4b3eb676250db6bae5
John Abramsson <John.abramsson@gmail.com>
2020-01-07 23:39:45 +0100

    First!

---
+++ Day1Solution1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1 {
+    class Program {
+        private static void Main(string[] args) {
+            const string textFile = @"c:\Tmp\input1.txt";
+
+            if (File.Exists(textFile)) {
+                var lines = File.ReadAllLines(textFile);
+
+                foreach (var line in lines) {
+                   Console.WriteLine(line); 
+                }
+                
+                
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
