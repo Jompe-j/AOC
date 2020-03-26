75c50081f43fc2a189306ff1f57fbd4904fe4dd2
John Abramsson <John.abramsson@gmail.com>
2020-03-26 14:31:41 +0100

    Added module fule calculator to calculate fule needed for added fuel.

--- Day1Solution1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Day1Solution1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,24 +4,16 @@ using System.IO;
 namespace ConsoleApp1 {
     class Program {
         private static void Main(string[] args) {
+        
+            var totalFuel = new FuleCalculator();
             const string textFile = @"c:\Tmp\input1.txt";
-            var sum = 0.0d; 
             if (File.Exists(textFile)) {
                 var lines = File.ReadAllLines(textFile);
-
-                foreach (var line in lines) {
-                    if (int.TryParse(line, out var number)) {
-                        sum += (Math.Floor(d: (double) (number / 3)) - 2);
-                        Console.WriteLine(sum);
-                        
-                    }
-
-                }
-                
-                
-                
-               
+                totalFuel.SimplePrintFuel(lines);
+                totalFuel.PrintTotalFuel(lines);            
             }
+
+       
         }
     }
 }
